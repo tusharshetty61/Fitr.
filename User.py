07ef445a3a15f3e13bd6de0e7536b5268c293b93
tusharshetty61 <tusharshetty61@gmail.com>
2022-02-12 18:25:18 +0530

    Devised a strategy to store user attributes

--- User.py
+++ User.py
@@ -1,8 +1,23 @@
 import streamlit as st
 
 class User:
+    
     def __init__(self, username):
-        self.user=user
+        self.user=username
         self.goals= {}
+        self.uinfo={}
         
-    def _
+    def update_user_goals(self,steps,calories,wellnessgoals,screentimegoals):
+        self.goals['steps']=steps 
+        self.goals['calories']=calories
+        self.goals['wellnessgoals']=wellnessgoals
+        self.goals['screentimegoals']=screentimegoals
+    
+    def update_user_info(self,ht,wt,activity,gender,age,hours):
+        self.uinfo['ht']=ht
+        self.uinfo['wt']=wt
+        self.uinfo['activity']=activity
+        self.uinfo['gender']=gender
+        self.uinfo['age']=age
+        self.uinfo['hours']=hours
+        
